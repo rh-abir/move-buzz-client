b1e84d3b154c25bf80edf3a0dcbb651e7a1fc03a
rh-abir <rhabir.co@gmail.com>
2023-07-09 18:40:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl  font-medium  text-red-300">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
