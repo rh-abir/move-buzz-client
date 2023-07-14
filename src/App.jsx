356842fad7a3538b7c38426cc51a79c5b83d63f3
rh-abir <rhabir.co@gmail.com>
2023-07-14 23:42:51 +0600

    add-plugin and make page files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomeScreen from "./Screens/HomeScreen";
+import About from "./Screens/About";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl  font-medium  text-red-300">Hello world!</h1>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element= {<HomeScreen></HomeScreen>}></Route> 
+      <Route path="/about" element = {<About></About>}></Route> 
+      <Route path="/about" element = {<About></About>}></Route> 
+    </Routes>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        header: "560px",
+        rate: "400px",
+      },
+
+      fontSize: {
+        h1: "2.6rem",
+      },
+
+      screens: {
+        xs: "475px",
+      },
+
+      colors: {
+        main: "#080a1a",
+        subMain: "#f20000",
+        dry: "#0b0f29",
+        star: "#ffb000",
+        text: "#c0c0c0",
+        border: "#4b5563",
+        dryGray: "#e0d5d5",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
